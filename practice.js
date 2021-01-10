a47d6d5ca47d26325e957d92bb69042cd0a3ddff
Kritika <kritikabindal30@gmail>
2021-01-10 14:43:04 +0530

    adding 2 files

---
+++ practice.js
@@ -0,0 +1,18 @@
+// const massmark = 78;
+// const heightmark = 1.69;
+
+// const massjohn = 92;
+// const heightjohn = 1.95;
+
+
+// const BMIMark = massmark / heightmark ** 2;
+// const BMIJohn = massjohn / (heightjohn * heightjohn);
+// console.log(BMIMark, BMIJohn);
+
+
+const firstname = "Jonas";
+const lastname = "singh"
+const birthyear = 1996;
+const year = 2087;
+const jonasnew = `I'm ${firstname}`;
+console.log(jonasnew);
